158491372600b02ac4eefd397cffe08a770712bc
Kalyn Robinson <kalyndrobinson@gmail.com>
2018-08-09 10:28:56 -0400

    Add missing options for release command

--- bin/commands/release.js
+++ bin/commands/release.js
@@ -3,7 +3,17 @@ import { release as releaseCmd } from "../../src/commands";
 export const release = config => ({
     command: "release",
     description: "Create symbolic links for plugin files and directories.",
-    options: { "-v, --verbose": "Output verbose error messages" },
+    options: {
+        "-v, --verbose": "Output verbose error messages",
+        "-s, --semver [version]": "Plugin version. Defaults to shinka.json.",
+        "-N, --vendor [vendor]":
+            "Vendor name, e.g. `shinka` in `shinka-cli`. Defaults to shinka.json.",
+        "-c, --code [code]":
+            "Plugin codename, e.g. `cli` in `shinka-cli`. Defaults to shinka.json.",
+        "-o, --output [path]": "Bundle filename. Should include file extension.",
+
+        "-d, --directory [path]": "Output directory and filename. Should include file extension."
+    },
     action: cmd => releaseCmd(cmd, config),
     examples: [
         {
